esrille/ibus-hiragana

commit e457cf496e8e7aa4d3b7f23175dfdd992523003c

Support Kanji replacement with kana suffixes following a Kanji stem.
dic/Makefile.am
dic/restrained.1.dic
dic/restrained.2.dic
dic/restrained.3.dic
dic/restrained.4.dic
dic/restrained.5.dic
dic/restrained.6.dic
dic/restrained.7.dic
dic/restrained.9.dic
dic/restrained.dic
dic_tools/dic.py
dic_tools/makedic
dic_tools/restrain.py
dic_tools/zyouyou-kanji.csv : Include inflection classes
engine/dictionary.py
engine/engine.py

engine/dictionary.py
@@ -23,6 +23,7 @@ import re
 logger = logging.getLogger(__name__)
 
 _re_not_yomi = re.compile(r'[^ぁ-ゖァ-ー―]')
+_dic_ver = "v0.4.0"
 
 class Dictionary:
 
@@ -41,30 +42,35 @@ class Dictionary:
 
         # Load Katakana dictionary first so that Katakana words come after Kanji words.
         katakana_path = os.path.join(os.getenv('IBUS_REPLACE_WITH_KANJI_LOCATION'), 'katakana.dic')
-        self.__load_dict(self.__dict_base, katakana_path);
+        self.__load_dict(self.__dict_base, katakana_path)
 
         # Load system dictionary
-        self.__load_dict(self.__dict_base, path);
+        self.__load_dict(self.__dict_base, path)
 
         # Load private dictionary
         self.__dict = self.__dict_base.copy()
 
         my_path = os.path.expanduser('~/.local/share/ibus-replace-with-kanji/my.dic')
-        self.__load_dict(self.__dict, my_path, 'a+');
+        self.__load_dict(self.__dict, my_path, 'a+')
 
         base = os.path.basename(path)
         if base:
             self.__orders_path = os.path.expanduser('~/.local/share/ibus-replace-with-kanji')
             self.__orders_path = os.path.join(self.__orders_path, base)
-            self.__load_dict(self.__dict, self.__orders_path, 'a+');
+            self.__load_dict(self.__dict, self.__orders_path, 'a+', version_checked=False)
 
-    def __load_dict(self, dict, path, mode='r'):
+    def __load_dict(self, dict, path, mode='r', version_checked = True):
         with open(path, mode) as file:
-            file.seek(0, 0)
             for line in file:
                 line = line.strip(' \n/')
-                if not line or line[0] == ';':
-                    continue;
+                if line[0] == ';':
+                    if line == "; " + _dic_ver + "\n":
+                        version_checked = True
+                    continue
+                if not line:
+                    continue
+                if not version_checked:
+                    continue
                 p = line.split(' ', 1)
                 yomi = p[0]
                 cand = p[1].strip(' \n/').split('/')
@@ -90,59 +96,158 @@ class Dictionary:
     def next(self):
         if self.__no + 1 < len(self.__cand):
             self.__no += 1
-        return self.__str(self.__cand[self.__no]);
+        return self.__str(self.__cand[self.__no])
 
     def previous(self):
         if 0 < self.__no:
             self.__no -= 1
-        return self.__str(self.__cand[self.__no]);
+        return self.__str(self.__cand[self.__no])
 
     def current(self):
         if self.__yomi:
-            return self.__str(self.__cand[self.__no]);
+            return self.__str(self.__cand[self.__no])
         return ''
 
     def set_current(self, index):
         index = int(index)
         if self.__yomi and 0 <= index and index < len(self.__cand):
-            self.__no = index;
+            self.__no = index
 
     def reading(self):
         return self.__yomi
 
     def cand(self):
         if self.__yomi:
-            return self.__cand;
+            return self.__cand
         return []
 
-    def lookup(self, yomi):
+    def __match(self, okuri, yomi):
+        if okuri and 0 <= "iIkKgsStnbmrw".find(okuri[-1]):
+            suffix = okuri[-1]
+            okuri = okuri[:-1]
+        else:
+            suffix = ''
+        l = min(len(okuri), len(yomi))
+        if not yomi.startswith(okuri[:l]):
+            return False
+        okuri = okuri[l:]
+        if okuri:
+            return True
+        yomi = yomi[l:]
+        if not suffix or not yomi:
+            return True
+        if suffix == 'i' or suffix == 'I':
+            if 2 <= len(yomi):
+                if yomi.startswith("かろ"):
+                    return True
+                if yomi.startswith("かっ"):
+                    return True
+                if yomi.startswith("けれ"):
+                    return True
+                if yomi.startswith("かれ"):
+                    return True
+            if 0 <= "けういさかく".find(yomi[0]):
+                return True
+            if suffix == 'I':   # 連体詞「大きな」、「小さな」。
+                if yomi[0] == 'な':
+                    return True
+            return False
+        if suffix == 'k':
+            return 0 <= "かきくけこい".find(yomi[0])
+        if suffix == 'K':   # 「いく」のみ
+            return 0 <= "かきくけこっ".find(yomi[0])
+        if suffix == 'g':
+            return 0 <= "がぎぐげごい".find(yomi[0])
+        if suffix == 's':
+            return 0 <= "さしすせそ".find(yomi[0])
+        if suffix == 'S':   # サ行変格活用
+            # see http://ci.nii.ac.jp/naid/40005174758, http://ci.nii.ac.jp/naid/40016557492
+            return 0 <= "しすせ".find(yomi[0])
+        if suffix == 't':
+            return 0 <= "たちつてとっ".find(yomi[0])
+        if suffix == 'n':
+            return 0 <= "なにぬねのん".find(yomi[0])
+        if suffix == 'b':
+            return 0 <= "ばびぶべぼん".find(yomi[0])
+        if suffix == 'm':
+            return 0 <= "まみむめもん".find(yomi[0])
+        if suffix == 'r':
+            return 0 <= "らりるれろっ".find(yomi[0])
+        if suffix == 'w':
+            return 0 <= "わいうえおっ".find(yomi[0])
+        return False
+
+    def lookup(self, yomi, pos):
         self.reset()
-        size = len(yomi)
-        for i in range(size - 1, -1, -1):
-            y = yomi[i:size]
-            if _re_not_yomi.search(y):
-                break;
-            if y in self.__dict:
-                self.__yomi = y
-                self.__cand = self.__dict[y]
-                self.__no = 0
+        suffix = yomi[:pos].find('―')
+        if suffix <= 0:
+            size = pos
+            for i in range(size - 1, -1, -1):
+                if _re_not_yomi.match(yomi[i]):
+                    break
+                y = yomi[i:size]
+                if y in self.__dict:
+                    self.__yomi = y
+                    self.__cand = self.__dict[y]
+                    self.__no = 0
+                    self.__order = list()
+            return self.current()
+
+        # Process suffix
+        size = suffix + 1
+        y = yomi
+        for i in range(len(yomi[size:])):
+            if _re_not_yomi.match(yomi[size + i]):
+                y = y[:size + i]
+                break
+        for i in range(size - 2, -1, -1):
+            if _re_not_yomi.match(y[i]):
+                break
+            if y[i:size] in self.__dict:
+                cand = list()
+                order = list()
+                n = 0
+                for c in self.__dict[y[i:size]]:
+                    p = self.__match(c[1:], y[size:])
+                    logger.debug("lookup: %s %s => %d", c, y[size:], p)
+                    c = c[0] + yomi[size:pos]
+                    if p and not c in cand:
+                        cand.append(c)
+                        order.append(n)
+                    n += 1
+                if cand:
+                    self.__yomi = yomi[i:pos]
+                    self.__cand = cand
+                    self.__no = 0
+                    self.__order = order
         return self.current()
 
     def confirm(self):
         if not self.__yomi or self.__no == 0:
             return
+
+        # Get a copy of the original candidate list
+        yomi = self.__yomi
+        no = self.__no
+        if self.__order:
+            yomi = yomi[:yomi.find('―') + 1]
+            no = self.__order[no]
+            cand = self.__dict[yomi][:]
+        else:
+            cand = self.__cand[:]
+
         # Update the order of the candidates.
-        cand = self.__cand[:]
-        last = cand[self.__no]
-        cand.remove(last)
-        cand.insert(0, last)
-        self.__dict[self.__yomi] = cand
-        self.__dirty = True;
+        first = cand[no]
+        cand.remove(first)
+        cand.insert(0, first)
+        self.__dict[yomi] = cand
+        self.__dirty = True
 
     def save_orders(self):
         if not self.__dirty:
             return
         with open(self.__orders_path, 'w') as file:
+            file.write("; " + _dic_ver + "\n")
             for yomi, cand in self.__dict.items():
                 if not yomi in self.__dict_base or cand != self.__dict_base[yomi]:
                     file.write(yomi + " /" + '/'.join(cand) + "/\n")

dic/Makefile.am
@@ -6,6 +6,8 @@ dic_DATA = \
 	restrained.4.dic \
 	restrained.5.dic \
 	restrained.6.dic \
+	restrained.7.dic \
+	restrained.9.dic \
 	katakana.dic \
 	$(NULL)
 
@@ -19,6 +21,8 @@ EXTRA_DIST = \
 	restrained.4.dic \
 	restrained.5.dic \
 	restrained.6.dic \
+	restrained.7.dic \
+	restrained.9.dic \
 	katakana.dic \
 	COPYING
 	$(NULL)

dic/restrained.1.dic
@@ -58,9 +58,9 @@
 ;; して作成しました。加除のご希望は、理由を添えて skk@ring.gr.jp
 ;; まで。
 ;;
-あ― /空/上/
+あ― /空k/空け/上げ/上がr/
 あお /青/
-あお― /青/
+あお― /青i/
 あおいし /青石/
 あおき /青木/
 あおくさ /青草/
@@ -71,7 +71,7 @@
 あおもり /青森/
 あおやま /青山/
 あか /赤/
-あか― /赤/
+あか― /赤i/赤らm/赤らめ/
 あかがい /赤貝/
 あかぎ /赤木/
 あかこほん /赤小本/
@@ -88,7 +88,7 @@
 あまくさ /天草/
 あまだ /天田/
 あめ /雨/
-い― /生/入/
+い― /生き/生かs/生け/入r/入れ/
 いし /石/
 いしかわ /石川/
 いしこ /石子/
@@ -130,7 +130,7 @@
 いっぽん /一本/
 いっぽんぎ /一本気/
 いつ /一/五/
-いつ― /五/
+いつ― /五つ/
 いつか /五日/
 いつき /五木/
 いと /糸/
@@ -139,7 +139,7 @@
 いぬめ /犬目/
 いぬやま /犬山/
 う /右/雨/
-う― /生/
+う― /生まれ/生m/
 うえ /上/
 うえだ /上田/
 うえむら /上村/
@@ -152,7 +152,7 @@
 えんくう /円空/
 えんぽん /円本/
 お /小/
-お― /下/
+お― /下ろs/下り/
 おう /王/
 おうじ /王子/
 おうじょ /王女/
@@ -161,7 +161,7 @@
 おうど /王土/
 おうりつ /王立/
 おお /大/
-おお― /大/
+おお― /大きI/大いに/
 おおいし /大石/
 おおうえ /大上/
 おおかわ /大川/
@@ -283,7 +283,7 @@
 くさき /草木/
 くさばな /草花/
 くじ /九字/
-くだ― /下/
+くだ― /下r/下s/下さr/
 くち /口/
 くちき /口木/
 くるま /車/
@@ -314,7 +314,7 @@
 こがね /小金/
 こぐち /小口/
 ここの /九/
-ここの― /九/
+ここの― /九つ/
 ここのか /九日/
 こしょうがつ /小正月/
 こだま /小玉/
@@ -338,7 +338,7 @@
 ごひゃく /五百/
 ごもく /五目/
 さ /左/
-さ― /下/
+さ― /下げ/下がr/
 さき /先/
 さゆう /左右/
 さん /三/山/
@@ -436,7 +436,7 @@
 しらつち /白土/
 しらやま /白山/
 しろ /白/
-しろ― /白/
+しろ― /白i/
 しろいし /白石/
 しろき /白木/
 しろこ /白子/
@@ -554,7 +554,7 @@
 そん /村/
 そんりつ /村立/
 た /田/
-た― /足/立/
+た― /足り/足r/足s/立t/立て/
 たい /大/
 たいか /大火/
 たいき /大気/
@@ -574,7 +574,7 @@
 たけむら /竹村/
 たけもと /竹本/
 たご /田子/
-ただ― /正/
+ただ― /正しi/正s/
 たま /玉/
 たまがわ /玉川/
 たまがわじょうすい /玉川上水/
@@ -584,7 +584,7 @@
 たまむし /玉虫/
 たむら /田村/
 たやま /田山/
-だ― /出/
+だ― /出s/
 だい /大/
 だいえん /大円/
 だいおう /大王/
@@ -607,7 +607,7 @@
 だんし /男子/
 だんじょ /男女/
 ち /千/
-ちい― /小/
+ちい― /小さI/
 ちから /力/
 ちからいし /力石/
 ちく /竹/
@@ -698,7 +698,7 @@
 なかやま /中山/
 なご /名子/
 なな /七/
-なな― /七/
+なな― /七つ/
 なの /七/
 なのか /七日/
 なま /生/
@@ -740,9 +740,9 @@
 ねんげつ /年月/
 ねんじゅう /年中/
 ねんねん /年々/年年/
-のぼ― /上/
-は― /生/
-はい― /入/
+のぼ― /上r/
+は― /生え/生やs/
+はい― /入r/
 はく /白/
 はくう /白雨/
 はくさん /白山/
@@ -769,7 +769,7 @@
 はなみず /花水/
 はなみずき /花水木/
 はなみづき /花見月/
-はや― /早/
+はや― /早i/早まr/早め/
 はやかわ /早川/
 はやくち /早口/
 はやし /林/
@@ -781,7 +781,7 @@
 ひた /日田/
 ひだり /左/
 ひと /一/人/
-ひと― /一/
+ひと― /一つ/
 ひとけ /人気/
 ひとみ /人見/
 ひなか /日中/
@@ -799,7 +799,7 @@
 ひゃくねん /百年/
 ひゃっか /百花/
 ふた /二/
-ふた― /二/
+ふた― /二つ/
 ふたこ /二子/
 ふたごやま /二子山/
 ぶん /文/
@@ -838,11 +838,11 @@
 まちじゅう /町中/
 まちだ /町田/
 まちなか /町中/
-まな― /学/
-まる― /円/
+まな― /学b/
+まる― /円i/
 まるやま /円山/
 み /三/
-み― /見/三/
+み― /見/見え/見せ/三つ/
 みかづき /三日月/
 みかみ /三上/
 みき /三木/
@@ -859,7 +859,7 @@
 みずのて /水の手/
 みた /三田/
 みたむら /三田村/
-みっ― /三/
+みっ― /三つ/
 みっか /三日/
 みっかてんか /三日天下/
 みほん /見本/
@@ -869,12 +869,12 @@
 みょうじ /名字/
 みょうもく /名目/
 む /六/
-む― /六/
+む― /六つ/
 むい /六/
 むいか /六日/
 むし /虫/
 むしけ /虫気/
-むっ― /六/
+むっ― /六つ/
 むら /村/
 むらかみ /村上/
 むらた /村田/
@@ -906,10 +906,10 @@
 もん /文/
 もんじ /文字/
 や /八/
-や― /八/
+や― /八つ/
 やぎ /八木/
-やす― /休/
-やっ― /八/
+やす― /休m/休まr/休め/
+やっ― /八つ/
 やつくち /八口/
 やま /山/
 やまかわ /山川/
@@ -930,10 +930,10 @@
 ゆう /右/夕/
 ゆうひ /夕日/
 よ /四/
-よ― /四/
+よ― /四つ/
 よう /八/
 ようか /八日/
-よっ― /四/
+よっ― /四つ/
 よっか /四日/
 よん /四/
 りき /力/

dic/restrained.2.dic
@@ -58,10 +58,10 @@
 ;; して作成しました。加除のご希望は、理由を添えて skk@ring.gr.jp
 ;; まで。
 ;;
-あ― /空/上/会/合/当/明/
+あ― /空k/空け/上げ/上がr/会w/合w/合わs/合わせ/当たr/当て/明かり/明け/明k/明くる/明かs/
 あいだ /間/
 あお /青/
-あお― /青/
+あお― /青i/
 あおいし /青石/
 あおき /青木/
 あおくさ /青草/
@@ -77,7 +77,7 @@
 あおもりし /青森市/
 あおやま /青山/
 あか /赤/
-あか― /赤/明/
+あか― /赤i/赤らm/赤らめ/明るi/明るm/明らm/
 あかがい /赤貝/
 あかぎ /赤木/
 あかげ /赤毛/
@@ -96,7 +96,7 @@
 あかめ /赤目/
 あかもん /赤門/
 あき /秋/
-あき― /明/
+あき― /明らか/
 あきがわ /秋川/
 あきた /秋田/
 あきたけん /秋田犬/
@@ -117,7 +117,7 @@
 あし /足/
 あたま /頭/
 あたまきん /頭金/
-あたら― /新/
+あたら― /新しi/
 あと /後/
 あときん /後金/
 あとさく /後作/
@@ -130,13 +130,13 @@
 あまだ /天田/
 あまの /天野/
 あめ /雨/
-あゆ― /歩/
+あゆ― /歩m/
 あら /新/
-あら― /新/
+あら― /新た/
 あらた /新田/
 あらて /新手/
-ある― /歩/
-い― /生/入/言/行/
+ある― /歩k/
+い― /生き/生かs/生け/入r/入れ/言w/行K/
 いえ /家/
 いけ /池/
 いけうち /池内/
@@ -256,7 +256,7 @@
 いっぽんぎ /一本気/
 いっぽんみち /一本道/
 いつ /一/五/
-いつ― /五/
+いつ― /五つ/
 いつか /五日/
 いつき /五木/
 いと /糸/
@@ -303,7 +303,7 @@
 いんよう /引用/
 いんりょく /引力/
 う /右/雨/
-う― /生/売/
+う― /生まれ/生m/売r/売れ/
 うえ /上/
 うえだ /上田/
 うえの /上野/
@@ -317,11 +317,11 @@
 うきょう /右京/
 うご /雨後/
 うし /牛/
-うし― /後/
+うし― /後ろ/
 うしんしつ /右心室/
 うすい /雨水/
 うた /歌/
-うた― /歌/
+うた― /歌w/
 うたかい /歌会/
 うたがわ /歌川/
 うち /内/
@@ -381,7 +381,7 @@
 えんぽん /円本/
 えんらい /遠来/
 お /小/
-お― /下/
+お― /下ろs/下り/
 おう /王/黄/
 おうぎょく /黄玉/
 おうけ /王家/
@@ -404,7 +404,7 @@
 おうどうらくど /王道楽土/
 おうりつ /王立/
 おお /大/
-おお― /大/多/
+おお― /大きI/大いに/多i/
 おおいし /大石/
 おおいちばん /大一番/
 おおうえ /大上/
@@ -467,9 +467,9 @@
 おがわ /小川/
 おぎ /小木/
 おぐるま /小車/
-おこな― /行/
-おし― /教/
-おそ― /教/
+おこな― /行w/
+おし― /教え/
+おそ― /教わr/
 おだ /小田/
 おだに /小谷/
 おと /音/
@@ -477,11 +477,11 @@
 おとうとぶん /弟分/
 おとこ /男/
 おとこやま /男山/
-おな― /同/
+おな― /同じ/
 おの /小野/
 おのだ /小野田/
 おのでら /小野寺/
-おも― /思/
+おも― /思w/
 おや /親/
 おやかた /親方/
 おやがいしゃ /親会社/
@@ -504,7 +504,7 @@
 おんな /女/
 おんめい /音名/
 か /科/下/火/花/日/夏/家/歌/
-か― /書/買/
+か― /書k/買w/
 かい /回/貝/会/海/絵/
 かいおうせい /海王星/
 かいが /絵画/
@@ -549,7 +549,7 @@
 かいわ /会話/
 かいわたい /会話体/
 かいわぶん /会話文/
-かえ― /帰/
+かえ― /帰r/帰s/
 かお /顔/
 かおう /花王/
 かがく /科学/歌学/
@@ -585,9 +585,9 @@
 かせい /火星/
 かせん /火線/
 かぜ /風/
-かぞ― /数/
+かぞ― /数え/
 かた /形/方/
-かた― /語/
+かた― /語r/語らw/
 かたい /歌体/
 かたち /形/
 かたな /刀/
@@ -638,7 +638,7 @@
 かめい /家名/下名/
 かもく /科目/
 かもん /家門/
-かよ― /通/
+かよ― /通w/
 かよう /火曜/
 かようび /火曜日/
 から /空/
@@ -657,7 +657,7 @@
 かわむら /川村/
 かわもと /川本/
 かん /間/
-かんが― /考/
+かんが― /考え/
 かんさく /間作/
 かんしょく /間食/間色/
 かんどう /間道/
@@ -744,7 +744,7 @@
 がんぽん /元本/
 がんらい /元来/
 き /気/木/汽/記/帰/黄/
-き― /切/聞/
+き― /切r/切れ/聞k/聞こえ/
 きいつ /帰一/
 きうち /木内/
 ききゅう /帰休/
@@ -893,7 +893,7 @@
 ぎょくたい /玉体/
 ぎょにく /魚肉/
 く /九/口/工/
-く― /食/組/来/
+く― /食w/組m/来る/
 くう /空/
 くうかい /空海/
 くうかん /空間/
@@ -919,7 +919,7 @@
 くさば /草場/
 くさばな /草花/
 くじ /九字/
-くだ― /下/
+くだ― /下r/下s/下さr/
 くち /口/
 くちえ /口絵/
 くちき /口木/
@@ -937,7 +937,7 @@
 くよう /九曜/
 くるま /車/
 くろ /黒/
-くろ― /黒/
+くろ― /黒i/
 くろいし /黒石/
 くろいわ /黒岩/
 くろかわ /黒川/
@@ -1184,7 +1184,7 @@
 こぐち /小口/
 ここう /戸口/
 ここの /九/
-ここの― /九/
+ここの― /九つ/
 ここのか /九日/
 ここまい /古々米/
 こころ /心/
@@ -1204,7 +1204,7 @@
 こじん /古人/
 こすう /戸数/
 こせい /古生/
-こた― /答/
+こた― /答え/
 こたい /古体/
 こだま /小玉/
 こちず /古地図/
@@ -1232,7 +1232,7 @@
 こぶん /古文/子分/
 こほん /小本/古本/
 こま /小間/
-こま― /細/
+こま― /細か/細かi/
 こまい /古米/
 こまえ /小前/
 こまち /小町/
@@ -1322,7 +1322,7 @@
 ごんご /言語/
 ごんじょう /言上/
 さ /左/作/
-さ― /下/
+さ― /下げ/下がr/
 さい /才/細/西/
 さいう /細雨/
 さいか /西下/
@@ -1444,7 +1444,7 @@
 さんり /三里/
 さんりん /山林/
 し /子/四/糸/止/市/思/紙/自/
-し― /知/
+し― /知r/
 しおう /四王/
 しおうてん /四王天/
 しおん /子音/
@@ -1476,7 +1476,7 @@
 しせい /四声/
 しそく /四足/
 した /下/
-した― /親/
+した― /親しi/親しm/
 したえ /下絵/
 したかた /下方/
 したじ /下地/
@@ -1704,9 +1704,9 @@
 しらは /白羽/
 しらやま /白山/
 しりつ /市立/
-しる― /記/
+しる― /記s/
 しろ /白/
-しろ― /白/
+しろ― /白i/
 しろいし /白石/
 しろうま /白馬/
 しろき /白木/
@@ -1970,8 +1970,8 @@
 すうふん /数分/
 すうめい /数名/
 すうり /数理/
-すく― /少/
-すこ― /少/
+すく― /少なi/
+すこ― /少し/
 ず /図/頭/
 ずえ /図絵/
 ずが /図画/
@@ -2173,7 +2173,7 @@
 そんない /村内/
 そんりつ /村立/
 た /田/多/太/
-た― /足/立/食/
+た― /足り/足r/足s/立t/立て/食べ/
 たい /体/大/太/台/
 たいか /大家/大火/
 たいかい /大会/大海/
@@ -2215,7 +2215,7 @@
 たう /多雨/
 たうた /田歌/
 たか /高/
-たか― /高/
+たか― /高i/高まr/高め/
 たかいし /高石/
 たかき /高木/
 たかぎ /高木/
@@ -2261,7 +2261,7 @@
 たそく /多足/
 たた /多々/
 ただ /多田/
-ただ― /正/直/
+ただ― /正しi/正s/直ちに/
 ただい /多大/
 たどく /多読/
 たに /谷/
@@ -2278,7 +2278,7 @@
 たねん /多年/
 たねんせい /多年生/
 たねんそう /多年草/
-たの― /楽/
+たの― /楽しi/楽しm/
 たばい /多売/
 たぶん /多分/多聞/
 たほう /多方/
@@ -2295,7 +2295,7 @@
 たもうさく /多毛作/
 たやま /田山/
 たよう /多用/
-だ― /出/
+だ― /出s/
 だい /台/大/弟/
 だいえん /大円/
 だいおう /大黄/大王/
@@ -2347,9 +2347,9 @@
 だんじょ /男女/
 だんせい /男声/
 ち /千/地/池/知/
-ちい― /小/
+ちい― /小さI/
 ちか /地下/
-ちか― /近/
+ちか― /近い/
 ちかしつ /地下室/
 ちかすい /地下水/
 ちかどう /地下道/
@@ -2541,14 +2541,14 @@
 つきばん /月番/
 つきみ /月見/
 つきみそう /月見草/
-つく― /作/
+つく― /作r/
 つち /土/
 つちけいろ /土気色/
 つちだ /土田/
 つちやま /土山/
 つの /角/
 つのぎ /角木/
-つよ― /強/
+つよ― /強i/強まr/強め/
 つよき /強気/
 て /手/
 てがた /手形/
@@ -2644,7 +2644,7 @@
 でんわぐち /電話口/
 でんわせん /電話線/
 と /十/土/戸/図/
-と― /止/
+と― /止まr/止め/
 とう /頭/刀/冬/当/東/答/読/
 とううん /東雲/
 とうかい /東海/
@@ -2700,7 +2700,7 @@
 とうよう /当用/
 とうらい /当来/
 とお /十/
-とお― /遠/通/
+とお― /遠i/通r/通s/
 とおか /十日/
 とおかまち /十日町/
 とおの /遠野/
@@ -2803,7 +2803,7 @@
 どよう /土曜/土用/
 どようび /土曜日/
 な /名/
-な― /鳴/
+な― /鳴k/鳴r/鳴らs/
 ない /内/
 ないか /内科/
 ないかい /内海/
@@ -2828,10 +2828,10 @@
 ないよう /内用/
 ないりょく /内力/
 なお /直/
-なお― /直/
+なお― /直s/直r/
 なおき /直木/
 なか /中/
-なか― /半/
+なか― /半ば/
 なかうみ /中海/
 なかがわ /中川/
 なかぎょう /中京/
@@ -2854,7 +2854,7 @@
 なかむら /中村/
 なかやま /中山/
 なが /長/
-なが― /長/
+なが― /長i/
 ながしかく /長四角/
 ながた /長田/
 ながつき /長月/
@@ -2872,7 +2872,7 @@
 なつめ /夏目/
 なつやま /夏山/
 なな /七/
-なな― /七/
+なな― /七つ/
 なないろ /七色/
 なに /何/
 なにぶん /何分/
@@ -3045,16 +3045,16 @@
 のはら /野原/
 のば /野場/
 のばな /野花/
-のぼ― /上/
+のぼ― /上r/
 のま /野間/
 のみず /野水/
 のむぎ /野麦/
 のむら /野村/
 のやま /野山/
 は /羽/
-は― /生/晴/
-はい― /入/
-はか― /計/
+は― /生え/生やs/晴れ/晴らs/
+はい― /入r/
+はか― /計r/計らw/
 はく /白/
 はくう /白雨/
 はくうん /白雲/
@@ -3082,9 +3082,9 @@
 はくまい /白米/
 はくや /白夜/
 はくわ /白話/
-はし― /走/
+はし― /走r/
 はじろ /羽白/
-はず― /外/
+はず― /外s/外れ/
 はち /八/
 はちおうじ /八王子/
 はちがつ /八月/
@@ -3103,7 +3103,7 @@
 はっぴゃくやちょう /八百八町/
 はっぽう /八方/
 はな /花/
-はな― /話/
+はな― /話s/
 はながた /花形/
 はなきん /花金/
 はなし /話/
@@ -3121,7 +3121,7 @@
 はは /母/
 ははこ /母子/
 はむら /羽村/
-はや― /早/
+はや― /早i/早まr/早め/
 はやうま /早馬/
 はやかわ /早川/
 はやくち /早口/
@@ -3210,8 +3210,8 @@
 ばんば /番場/
 ばんめ /番目/
 ひ /火/日/
-ひ― /引/
-ひか― /光/
+ひ― /引k/引け/
+ひか― /光r/
 ひかた /日方/
 ひかり /光/
 ひがし /東/
@@ -3223,7 +3223,7 @@
 ひだか /日高/
 ひだり /左/
 ひと /一/人/
-ひと― /一/
+ひと― /一つ/
 ひとけ /人気/
 ひとこと /一言/
 ひとみ /人見/
@@ -3251,7 +3251,7 @@
 ひゃっこう /百行/
 ひる /昼/
 ひるま /昼間/
-ひろ― /広/
+ひろ― /広i/広まr/広め/広がr/広げ/
 ひろくち /広口/
 ひろた /広田/
 ひろの /広野/
@@ -3284,11 +3284,11 @@
 ふうりんかざん /風林火山/
 ふし /父子/
 ふた /二/
-ふた― /二/
+ふた― /二つ/
 ふたかた /二方/
 ふたこ /二子/
 ふたごやま /二子山/
-ふと― /太/
+ふと― /太i/太r/
 ふな /船/
 ふなき /船木/
 ふなだいく /船大工/
@@ -3299,7 +3299,7 @@
 ふゆき /冬木/
 ふゆの /冬野/
 ふる /古/
-ふる― /古/
+ふる― /古i/古す/
 ふるいえ /古家/
 ふるいけ /古池/
 ふるかわ /古川/
@@ -3383,7 +3383,7 @@
 ほし /星/
 ほすう /歩数/
 ほそ /細/
-ほそ― /細/
+ほそ― /細i/細r/
 ほそかわ /細川/
 ほそき /細木/
 ほそぐち /細口/
@@ -3455,7 +3455,7 @@
 ぼせん /母船/母線/
 ぼたい /母体/
 ま /間/
-ま― /交/
+ま― /交じr/交ざr/交ぜ/
 まい /米/毎/
 まいあさ /毎朝/
 まいかい /毎回/
@@ -3485,16 +3485,16 @@
 まぐち /間口/
 まさ /正/
 まさき /正木/
-まじ― /交/
+まじ― /交わr/交え/
 まち /町/
 まちかた /町方/
 まちこうば /町工場/
 まちじゅう /町中/
 まちだ /町田/
 まちなか /町中/
-まな― /学/
+まな― /学b/
 まる /丸/
-まる― /円/丸/
+まる― /円i/丸i/丸め/
 まるいち /丸一/
 まるき /丸木/
 まるこ /丸子/
@@ -3504,7 +3504,7 @@
 まるほん /丸本/
 まるまる /丸々/
 まるやま /丸山/円山/
-まわ― /回/
+まわ― /回r/回s/
 まん /万/
 まんいち /万一/
 まんいつ /万一/
@@ -3519,7 +3519,7 @@
 まんもく /万目/
 まんりき /万力/
 み /三/
-み― /見/三/
+み― /見/見え/見せ/三つ/
 みいけ /三池/
 みかた /見方/
 みかづき /三日月/
@@ -3531,7 +3531,7 @@
 みず /水/
 みずあし /水足/
 みずえ /水絵/
-みずか― /自/
+みずか― /自ら/
 みずき /水木/
 みずくさ /水草/
 みずぐち /水口/
@@ -3549,7 +3549,7 @@
 みた /三田/
 みたむら /三田村/
 みち /道/
-みっ― /三/
+みっ― /三つ/
 みっか /三日/
 みっかてんか /三日天下/
 みなみ /南/
@@ -3573,7 +3573,7 @@
 みょうもく /名目/
 みょうや /明夜/
 む /六/
-む― /六/
+む― /六つ/
 むい /六/
 むいか /六日/
 むぎ /麦/
@@ -3581,7 +3581,7 @@
 むぎちゃ /麦茶/
 むし /虫/
 むしけ /虫気/
-むっ― /六/
+むっ― /六つ/
 むら /村/
 むらかみ /村上/
 むらた /村田/
@@ -3634,7 +3634,7 @@
 もくめ /木目/
 もくよう /木曜/
 もくようび /木曜日/
-もち― /用/
+もち― /用い/
 もっか /目下/
 もっこう /木工/
 もっこん /目今/
@@ -3666,7 +3666,7 @@
 もんない /門内/
 もんばん /門番/
 や /八/家/矢/夜/野/
-や― /八/
+や― /八つ/
 やかい /夜会/
 やかず /矢数/
 やかん /夜間/
@@ -3689,13 +3689,13 @@
 やしん /野心/
 やしんか /野心家/
 やじん /野人/
-やす― /休/
+やす― /休m/休まr/休め/
 やせい /野生/
 やぜん /夜前/
 やそう /野草/
 やちゅう /夜中/
 やちょう /野鳥/
-やっ― /八/
+やっ― /八つ/
 やつくち /八口/
 やないはら /矢内原/
 やの /矢野/
@@ -3733,7 +3733,7 @@
 やまもと /山本/山元/
 やらい /夜来/矢来/
 やわ /夜話/
-ゆ― /行/
+ゆ― /行k/
 ゆう /右/夕/友/
 ゆうがお /夕顔/
 ゆうしょく /夕食/
@@ -3744,7 +3744,7 @@
 ゆみ /弓/
 ゆみや /弓矢/
 よ /四/夜/
-よ― /四/読/
+よ― /四つ/読m/
 よう /八/用/曜/
 ようか /八日/
 ようかいち /八日市/
@@ -3763,13 +3763,13 @@
 ようび /曜日/
 ようぼく /用木/
 よぎしゃ /夜汽車/
-よっ― /四/
+よっ― /四つ/
 よっか /四日/
 よっかいち /四日市/
 よなか /夜中/
 よばん /夜番/
 よる /夜/
-よわ― /弱/
+よわ― /弱i/弱r/弱まr/弱め/
 よわき /弱気/
 よん /四/
 らい /来/
@@ -3847,4 +3847,4 @@
 ろくよう /六曜/
 ろっかく /六角/
 わ /話/
-わ― /分/
+わ― /分け/分かれ/分かr/分かt/

commit 659961e6bae5abdbf0b1dc6d7ead3d5293f27397

engine/dictionary.py (__load_dict) : Fix a bug introduced in e457cf496e8e7aa4d3b7f23175dfdd992523003c

engine/dictionary.py
@@ -61,6 +61,7 @@ class Dictionary:
 
     def __load_dict(self, dict, path, mode='r', version_checked = True):
         with open(path, mode) as file:
+            file.seek(0, 0)   # in case opened in the 'a+' mode
             for line in file:
                 line = line.strip(' \n/')
                 if line[0] == ';':