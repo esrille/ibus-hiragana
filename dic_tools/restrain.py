91d7843a1aafab3827cb3bc85184d7ba32959ab0
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2017-07-24 20:36:40 +0900

    Support Group 3 verb conjugation of '来る'.
    dic/restrained.2.dic
    dic/restrained.3.dic
    dic/restrained.4.dic
    dic/restrained.5.dic
    dic/restrained.6.dic
    dic/restrained.7.dic
    dic/restrained.9.dic
    dic/restrained.dic
    dic_tools/Makefile.am
    dic_tools/kahen.dic
    dic_tools/restrain.py

--- dic_tools/restrain.py
+++ dic_tools/restrain.py
@@ -64,6 +64,8 @@ if __name__ == '__main__':
     if 6 < grade:
         dict = dic.union(dict, dic.load('fuhyou.dic'))          # 常用漢字表・付表の熟語を追加。
     dict = dic.union(dic.zyouyou(grade), dict)                  # 常用漢字を追加
+    if 1 < grade:
+        dict = dic.union(dict, dic.load('kahen.dic'))           # カ変活用を追加
     dict = dic.union(zyosuusi, dict)                            # 助数詞を先頭に追加
     dict = dic.union(dict, reigai)                              # 例外を追加
     if 8 <= grade:
