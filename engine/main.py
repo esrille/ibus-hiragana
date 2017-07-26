0441548c0ef5d4a952af36eb469e4b550f02cd03
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2017-07-27 06:25:06 +0900

    Set version to 0.4.0
    README.md
    configure.ac
    engine/main.py

--- engine/main.py
+++ engine/main.py
@@ -39,7 +39,7 @@ class IMApp:
         self.__component = IBus.Component.new(
             "org.freedesktop.IBus.ReplaceWithKanji",
             "Replace With Kanji Input Method",
-            "0.3.0",
+            "0.4.0",
             "Apache",
             "Esrille Inc. <info@esrille.com>",
             "https://github.com/esrille/ibus-replace-with-kanji",
