b0b1facdf68337cbbec76a3b3a105457cc4a280d
Shiki Okasaka <Shiki.Okasaka@gmail.com>
2024-03-19 16:03:06 +0900

    Update copyright notice
    NOTICE
    engine/engine.py
    setup/Makefile.am

--- engine/engine.py
+++ engine/engine.py
@@ -3,7 +3,7 @@
 # Using source code derived from
 #   ibus-tmpl - The Input Bus template project
 #
-# Copyright (c) 2017-2023 Esrille Inc.
+# Copyright (c) 2017-2024 Esrille Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
